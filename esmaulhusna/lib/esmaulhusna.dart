fe268fbf217ca49fd61ab9bd62ee0019ac515e90
Cemal Karabulaklı <4073721+cemalkarabulakli@users.noreply.github.com>
2025-02-21 23:34:49 +0300

    refactor

--- esmaulhusna/lib/esmaulhusna.dart
+++ esmaulhusna/lib/esmaulhusna.dart
@@ -1,5 +1,3 @@
-import 'src/esmaulhusna_base.dart';
-
 export 'src/esmaulhusna_base.dart';
 
 // No need for platform-specific code since we're just providing data
--- esmaulhusna/lib/src/esmaulhusna_base.dart
+++ esmaulhusna/lib/src/esmaulhusna_base.dart
@@ -15,6 +15,14 @@ class EsmaulHusna {
     return _names[number - 1]['arabic'] as String;
   }
 
+  /// Get Bulgarian name for given number (1-99)
+  static String getBulgarianName(int number) {
+    if (number < 1 || number > 99) {
+      throw RangeError('Number must be between 1 and 99');
+    }
+    return _names[number - 1]['bulgarian'] as String;
+  }
+
   /// Get description for given number (1-99)
   static String getDescription(int number) {
     if (number < 1 || number > 99) {
@@ -33,15 +41,22 @@ class EsmaulHusna {
     return _names.map((name) => name['arabic'] as String).toList();
   }
 
+  /// Get list of all Bulgarian names
+  static List<String> getAllBulgarianNames() {
+    return _names.map((name) => name['bulgarian'] as String).toList();
+  }
+
   static const List<Map<String, String>> _names = [
     {
       'english': 'Ar-Rahman',
       'arabic': 'الرَّحْمَنُ',
+      'bulgarian': 'Всемилостивият',
       'description': 'The Most Gracious',
     },
     {
       'english': 'Ar-Raheem',
       'arabic': 'الرَّحِيمُ',
+      'bulgarian': 'Милосърдният',
       'description': 'The Most Merciful',
     },
     // Add all 99 names here...
